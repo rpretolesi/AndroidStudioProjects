e5c1fcc08ec492df704d24aeedb9679c403ce9be
RPRETOLESI <riccardo.pretolesi@gmail.com>
2015-01-14 14:55:51 +0100

    First Commit

---
+++ HowToRecipe/BE/src/main/java/com/example/RPRETOLESI/howtorecipe/BE/Profile.java
@@ -0,0 +1,18 @@
+package com.example.RPRETOLESI.howtorecipe.BE;
+
+/**
+ * Created by RPRETOLESI on 08/01/2015.
+ */
+public class Profile
+{
+    private String myData;
+
+    public String getData() {
+        return myData;
+    }
+
+    public void setData(String data) {
+        myData = data;
+    }
+
+}
